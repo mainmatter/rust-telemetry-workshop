0a978f403be71cb1c0534aac3763f5d505608efa
LukeMathWalker <20745048+LukeMathWalker@users.noreply.github.com>
2023-08-10 17:06:56 +0200

    Add welcome exercise

---
+++ exercises/00_intro/00_welcome/src/main.rs
@@ -0,0 +1,23 @@
+/// Welcome to this workshop!
+/// We'll be diving into the available telemetry solutions for Rust applications.
+/// 
+/// The workshop is structured as a series of exercises. 
+/// Each exercise is a standalone Rust project. An exercise is solved
+/// when it compiles and all its tests pass.
+/// You will have to replace intentional blanks (e.g. `____`) or fill in
+/// stubbed out functionality (e.g. `todo!()`) with your own code.
+/// 
+/// The exercises are ordered, building on top of each other to form a 
+/// coherent learning path. 
+/// 
+/// Enjoy!
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn starting_block() {
+        let msg = format!("I'm ready to __!");
+        assert_eq!(msg, "I'm ready to go!")
+    }
+}
---
+++ exercises/00_intro/00_welcome/target/debug/deps/welcome-99bdc4f2abdecdc3.d
@@ -0,0 +1,5 @@
+/Users/luca/code/telemetry-workshop/exercises/00_intro/00_welcome/welcome/target/debug/deps/welcome-99bdc4f2abdecdc3: src/main.rs
+
+/Users/luca/code/telemetry-workshop/exercises/00_intro/00_welcome/welcome/target/debug/deps/welcome-99bdc4f2abdecdc3.d: src/main.rs
+
+src/main.rs:
